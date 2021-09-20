cb84e3a67a5e5cbc6a0976917be47b1128ad38e3
Dafei Du <dud@oregonstate.edu>
2021-09-19 23:40:03 -0400

    add actor

--- Project1.fsx
+++ Project1.fsx
@@ -2,6 +2,7 @@
 #r "nuget: Akka.FSharp"
 #r "nuget: Akka.TestKit"
 
+
 open System
 open Akka.Actor
 open Akka.Configuration
@@ -12,6 +13,10 @@ open System.Text
 
 let system = ActorSystem.Create("FSharp")
 
+// input number of leading 0's we need 
+let k:int = int (fsi.CommandLineArgs |> Seq.item 1)
+let num_actor = 1024
+let rand = Random(1234)
 type WorkerMsg = 
     | Work of int
 
@@ -25,23 +30,46 @@ let sha256Hash (input : string) =
     |> Seq.map (fun c -> c.ToString("x2"))
     |> Seq.reduce (+)
 
+// test sha256Hash function 
+//let encoded = sha256Hash("dafei.du")
+//printfn ("test sha256 hash: %s\n") encoded
 
-let ascii (s : string) = 
+let rec ascii (s : string) = 
     
     let start:char = ' '
     let ends:char = '~'
     
     let startstring:string = string start
     let endstring:string = string ends
+    let isCorrect = String.forall (fun c -> c = ends)
 
     let ss:char = s.[s.Length - 1]
    
     if (ss = ends) then
-        s + startstring
-
+        ascii(s.[0..s.Length-2]) + startstring
     else
         let sss:int = int ss + 1
         s.[0..(s.Length - 2)] + string (Convert.ToChar(sss))
+
+let ascii_1 (s : string) = 
+    
+    let start:char = ' '
+    let ends:char = '~'
+    
+    let startstring:string = string start
+
+    let isCorrect = String.forall (fun c -> c = ends)
+
+    let ss:char = s.[s.Length - 1]
+   
+    if (isCorrect(s)) then
+        String.replicate (s.Length + 1) startstring
+    else 
+        ascii(s)
+
+// test ASCII_1 function 
+//let randomEncoded = ascii_1("a")
+//printfn("test ascii_1: %s\n") randomEncoded
 
 let rec leadzeros (h:string) = 
     if h.[0] <> '0' then
@@ -49,83 +77,52 @@ let rec leadzeros (h:string) =
     else 
         1 + leadzeros(h.[1..h.Length])
 
-let compute inpu = 
 
-    let workunit:int = 10000
+//let k = fsi.CommandLineArgs |> Seq.item 1
+//
+//printfn ("%A\n") k
+//let res = leadzeros(k)
+//printfn ("test leading zeros: %A\n") res
+
+let compute inpu = 
+    let workunit:int = 10
     let start:char = ' '
     let startstring:string = string start
     let mutable workends:string = startstring
-    
 
     while (workends.Length <= workunit) do
         let bitcoin:string = "yuhaoshi" + workends
         let hash = sha256Hash(bitcoin)
         let count = leadzeros(hash) 
         ///Seq.length(Seq.filter (fun x' -> x' = '0') hash.[0..workunit])
-
+//        printfn ("%s\n") workends 
         if (count = inpu) then
             printfn "%s %s" bitcoin hash
 
-        workends <- ascii(workends)
-
-compute 4
+        workends <- ascii_1(workends)
 
 
+//compute k
 
-
-let splitwork a = 
-    let numactor = 8
-    
-    [1..numactor]
-    |> List.map(fun id -> 
-                    spawn system ("actor" + string(id))
-                    <| fun mailbox ->
-                        let rec loop() = actor {
-                            let! msg = mailbox.Receive()
-                            
-                            match msg with
-                            | Work(input1) -> compute input1
-                                              mailbox.Sender() <! "Done"
-                            return! loop()
-                        }
-                        loop())
-
-
-//let master = spawn system "master" <| fun mailbox ->
-//    let rec loop() = actor {
-//        let! msg = mailbox.Receive()
-//        let mutable numactor = 0
-//
-//        match msg with
-//        |Assignjob(a)-> let re = splitwork a
-//                        for r in re do
-//                            Async.RunSynchronously(r,-1) |> ignore
-//                        mailbox.Sender() <! "Done"
-//
-//        return! loop()
-//    }
-//    loop()
-
-let input:int = int fsi.CommandLineArgs.[0]
-
-// input number of leading 0's
-//let k:int = int fsi.CommandLineArgs.[1]
-let k = int(fsi.CommandLineArgs |> Seq.item 2)
-//let boss = (master <? Assignjob(input))
-
+// using actor below
 type master(name) =
     inherit Actor()
     
     override x.OnReceive message =
         match message with
-        | :? string as msg -> sha256Hash msg k 
+        | :? int as k -> compute k
         | _-> failwith "unknown message"
-        
+ 
+   
 let master =
-    [1..1024]
+    [1..num_actor]
     |> List.map(fun id -> let property = [| string(id) :> obj|]
                           system.ActorOf(Props(typedefof<master>, property)))
-        
-Async.RunSynchronously(boss,-1) |> ignore
 
 
+
+
+for id = 1 to num_actor do
+    (rand.Next() % num_actor) |> List.nth master <! id
+
+system.Terminate |> ignore 

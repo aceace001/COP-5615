c43c047a42a4147813cb17d513a3139664da58bd
aceace001 <72894562+aceace001@users.noreply.github.com>
2021-09-21 02:13:17 -0400

    Update Project1.fsx

--- Project1.fsx
+++ Project1.fsx
@@ -12,8 +12,10 @@ open System.Text
 
 let system = ActorSystem.Create("FSharp")
 
+
+
 type WorkerMsg = 
-    | Work of int
+    | Work of int * int * int
 
 type MasterMsg = 
     | Assignjob of int
@@ -67,72 +69,97 @@ let rec leadzeros (h:string) =
     else 
         1 + leadzeros(h.[1..h.Length])
 
-let compute inpu = 
+let worker (mailbox:Actor<_>) = 
+    let rec loop () = actor {
+        let! msg = mailbox.Receive()
+        match msg with
+        | Work(starta, enda,input) -> 
+            let workunit:int = 3
+            let start:char = ' '
+            let startstring:string = string start
+            let mutable workends:string = startstring
+            let mutable lists : string list = []
+            
+            while (workends.Length <= workunit) do
+                let bitcoin:string = "yuhaoshi" + workends
+                lists <- bitcoin :: lists
+                
+                workends <- ascii_1(workends)
+            let names = lists |> List.toArray
+            ///let rand = new System.Random()
+            ///let nl = names.[rand.Next(names.Length)]
 
-    let workunit:int = 3
-    let start:char = ' '
-    let startstring:string = string start
-    let mutable workends:string = startstring
-    let mutable lists : string list = []
-    
+            for items in names.[starta .. enda] do 
+                let hash = sha256Hash(items)
+                let count = leadzeros(hash) 
+                if count = input then
+                    printfn "%s %s" items hash
+            
+            mailbox.Sender() <! "Done"
+            
+        return! loop ()
+    }
+    loop ()
 
-    while (workends.Length <= workunit) do
-        let bitcoin:string = "yuhaoshi" + workends
-        lists <- bitcoin :: lists
-        
-        workends <- ascii_1(workends)
-        
-    let names = lists |> List.toArray
-    ///let rand = new System.Random()
-    ///let nl = names.[rand.Next(names.Length)]
+let worker1 = spawn system "Worker1" worker
+let worker2 = spawn system "Worker2" worker
+let worker3 = spawn system "Worker3" worker
+let worker4 = spawn system "Worker4" worker
+let worker5 = spawn system "Worker5" worker
+let worker6 = spawn system "Worker6" worker
+let worker7 = spawn system "Worker7" worker
+let worker8 = spawn system "Worker8" worker
 
-    for items in names.[0 .. 10000] do 
-        let hash = sha256Hash(items)
-        let count = leadzeros(hash) 
-        if count = input then
-            printfn "%s %s" items hash
-
-compute 4
-
-
-
-
-let splitwork a = 
-    let numactor = 8
-    
-    [1..numactor]
-    |> List.map(fun id -> 
-                    spawn system ("actor" + string(id))
-                    <| fun mailbox ->
-                        let rec loop() = actor {
-                            let! msg = mailbox.Receive()
-                            
-                            match msg with
-                            | Work(input1) -> compute input1
-                                              mailbox.Sender() <! "Done"
-                            return! loop()
-                        }
-                        loop())
-
-
-let master = spawn system "master" <| fun mailbox ->
+let master (mailbox:Actor<_>) =
     let rec loop() = actor {
         let! msg = mailbox.Receive()
-        let mutable numactor = 0
+        let mutable nWorkLeft = 0
 
         match msg with
-        |Assignjob(a)-> let re = splitwork a
-                        for r in re do
-                            Async.RunSynchronously(r,-1) |> ignore
-                        mailbox.Sender() <! "Done"
+        |Assignjob(a)-> 
+            let workuni = 3
+            let input  = a
+            let numactor = 8
+
+            let worktotal = 95 * 95 * 95 
+
+            let workeach = worktotal / numactor
+
+            let start1: int = 1
+            let start2: int = start1 + workeach
+            let start3: int = start2 + workeach
+            let start4: int = start3 + workeach
+            let start5: int = start4 + workeach
+            let start6: int = start5 + workeach
+            let start7: int = start6 + workeach
+            let start8: int = start7 + workeach
+            
+            worker1 <! Work(start1,start2-1, a)
+            worker2 <! Work (start2,start3-1, a)
+            worker3 <! Work (start3,start4-1,a)
+            worker4 <! Work (start4,start5-1, a)
+            worker5 <! Work (start5,start6-1,a)
+            worker6 <! Work (start6,start7-1,a)
+            worker7 <! Work (start7,start8-1, a)
+            worker8 <! Work (start8,worktotal,a)
+            
+            mailbox.Sender() <! "Done"
+
+        ///|Ans ->
+
+           /// nWorkLeft <- nWorkLeft - 1
+           /// if nWorkLeft = 0 then
+           ///     Environment.Exit 1 
+            
 
         return! loop()
     }
     loop()
 
 
-let input:int = int fsi.CommandLineArgs.[0]
-let boss = (master <? Assignjob(input))
 
-Async.RunSynchronously(boss,-1) |> ignore
+let input = int (fsi.CommandLineArgs |> Seq.item 1)
+let masteref = spawn system "master" master
+masteref <! Assignjob(input)
 
+system.Terminate()
